425a3eab0d4542ef4201e7eb8a19f4bcaa21ee83
eyasir2047 <eyasir2047@gmail.com>
2023-10-18 01:51:45 +0600

    fragment_home frontend is done by abrar

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -17,17 +17,20 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
-        TextView home =(TextView) view.findViewById(R.id.home);
-        home.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_DEFAULT);
-                intent.setType("vnd.android-dir/mms-sms");
-                startActivity(intent);
-            }
-        });
-        return view;
+//
+//       TextView home =(TextView) view.findViewById(R.id.welcome);
+//        home.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_MAIN);
+//                intent.addCategory(Intent.CATEGORY_DEFAULT);
+//                intent.setType("vnd.android-dir/mms-sms");
+//                startActivity(intent);
+//            }
+//        });
+      return view;
+
+
     }
 
 
