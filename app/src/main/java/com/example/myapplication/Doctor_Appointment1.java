f0e5a9d3c927de090979167019c9a3d1a554bcec
eyasir2047 <eyasir2047@gmail.com>
2023-10-25 00:26:36 +0600

    image,name and desc -one intent to another by abrar

--- app/src/main/java/com/example/myapplication/Doctor_Appointment1.java
+++ app/src/main/java/com/example/myapplication/Doctor_Appointment1.java
@@ -6,8 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class Doctor_Appointment1 extends AppCompatActivity {
+
+
+    private TextView mTextViewDoctorName;
+    private TextView mTextViewDoctorDesc;
+    private ImageView mImageViewDoctorPhoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +25,28 @@ public class Doctor_Appointment1 extends AppCompatActivity {
         setContentView(R.layout.activity_doctor_appointment1);
         Button bt = (Button) findViewById (R.id.getAppointment);
 
+        mTextViewDoctorName=(TextView)findViewById(R.id.doctorName);
+        mTextViewDoctorDesc=(TextView)findViewById(R.id.doctorTitle);
+        mImageViewDoctorPhoto=(ImageView)findViewById(R.id.doctorPicture);
+
+        Intent intent=getIntent();
+        String inputDoctorName=intent.getStringExtra(Doctor_MyAdapter.key1);
+        String inputDoctorDesc=intent.getStringExtra(Doctor_MyAdapter.key2);
+        String inputDoctorPhoto=intent.getStringExtra(Doctor_MyAdapter.key3);
+
+       // int imageResource = getResources().getIdentifier(inputDoctorPhoto, "drawable", getPackageName());
+
+        mTextViewDoctorName.setText(inputDoctorName);
+        mTextViewDoctorDesc.setText(inputDoctorDesc);
+      //  mImageViewDoctorPhoto.setImageResource(imageResource);
+
+        Picasso.get().load(inputDoctorPhoto).into(mImageViewDoctorPhoto);
+
 
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 openAppoointment();
             }
         });
@@ -32,3 +60,5 @@ public class Doctor_Appointment1 extends AppCompatActivity {
 
 
 }
+
+
--- app/src/main/java/com/example/myapplication/Doctor_MyAdapter.java
+++ app/src/main/java/com/example/myapplication/Doctor_MyAdapter.java
@@ -19,6 +19,10 @@ public class Doctor_MyAdapter extends RecyclerView.Adapter<Doctor_MyViewHolder> 
     private List<Doctor_MyItem> Mylist;
     private Context context;
 
+    public static final String key1="msg";
+    public static final String key2="desc";
+    public static final String key3="photo";
+
     public Doctor_MyAdapter(List<Doctor_MyItem> mylist,Context context) {
         Mylist = mylist;
         this.context=context;
@@ -49,13 +53,43 @@ public class Doctor_MyAdapter extends RecyclerView.Adapter<Doctor_MyViewHolder> 
             }
         });
 
+
+        //bookbtn----------------
         holder.bookBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String doctor_name = myItemPosition.getHead();
+                String doctor_des = myItemPosition.getDesc();
+                String doctor_photo = myItemPosition.getImageUrl(); // Change data type to String
+
                 Intent intent = new Intent(view.getContext(), Doctor_Appointment1.class);
+                intent.putExtra(key1, doctor_name);
+                intent.putExtra(key2, doctor_des);
+                intent.putExtra(key3, doctor_photo);
+
                 view.getContext().startActivity(intent);
             }
         });
+
+
+
+//        //bookbtn----------------
+//        holder.bookBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String doctor_name= myItemPosition.getHead();
+//                String doctor_des=myItemPosition.getDesc();
+//                int doctor_photo=Integer.parseInt(myItemPosition.getImageUrl());
+//
+//                Intent intent = new Intent(view.getContext(), Doctor_Appointment1.class);
+//                intent.putExtra(key1,doctor_name);
+//                intent.putExtra(key2,doctor_des);
+//                intent.putExtra(key3,doctor_photo);
+//
+//
+//                view.getContext().startActivity(intent);
+//            }
+//        });
 
 
     }
