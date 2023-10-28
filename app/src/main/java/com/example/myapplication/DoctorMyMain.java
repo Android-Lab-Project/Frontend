acfd95abcb322fe487d78bba045d0689b6051e8c
eyasir2047 <eyasir2047@gmail.com>
2023-10-28 18:05:46 +0600

    docotrMylIST

---
+++ app/src/main/java/com/example/myapplication/DoctorMyMain.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MydoctorMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mydoctor_main);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/PatientConfirmationToDoctor.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientConfirmationToDoctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_confirmation_to_doctor);
+    }
+}
