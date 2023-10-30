035c8efb39e356642a7becbe2dfdce4d3aa255a2
Fahim Shakil <littlefahim2000@gmail.com>
2023-10-30 22:53:36 +0600

    Update StaticVariable.java

--- app/src/main/java/com/example/myapplication/StaticVariable.java
+++ app/src/main/java/com/example/myapplication/StaticVariable.java
@@ -11,7 +11,7 @@ public class StaticVariable {
 
 
 
-    public static String araf="http://192.168.0.111:8080";
+    public static String araf="http://api.healtechbd.space";
     public static String abrar="http://192.168.0.116:8080";
 
     public  static  String fahim="http://192.168.0.105:8080";
